464d75c945ecb964cc9a8c0b22eb6f929d82c5c6
catmorales <c.morales@rennesmetropole.fr>
2020-10-22 14:36:12 +0200

    update functions for cadastre_qgis

--- src/main/resources/data/function.sql
+++ src/main/resources/data/function.sql
@@ -1,36 +1,42 @@
---function intersect cadastre.edi_parc &&  limite_admin.quartier
-CREATE OR REPLACE FUNCTION limite_admin.intersect_ediparc_quartier(idparc VARCHAR)
-    RETURNS TABLE(numnom VARCHAR)
-AS $$
+---function intersect cadastre.edi_parc &&  limite_admin.quartier
+CREATE OR REPLACE FUNCTION limite_admin.rm_intersect_ediparc_quartier(IN idparc character varying)
+  RETURNS TABLE(numnom character varying) AS
+$BODY$
 BEGIN
     RETURN QUERY SELECT b.numnom
-        FROM  cadastre.edi_parc AS a , limite_admin.quartier AS b
-        WHERE a.id_parc = idParc AND st_intersects(a.shape, b.shape);
+        FROM  cadastre_qgis.geo_parcelle AS a , limite_admin.quartier AS b
+      WHERE a.geo_parcelle = idParc AND st_intersects(a.geom, b.shape);
 END;
-$$
-LANGUAGE 'plpgsql';
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
 
 --function intersect cadastre.edi_parc &&  urba_foncier.ads_secteur_instruction
-CREATE OR REPLACE FUNCTION urba_foncier.intersect_ediparc_adssecteurinstruction(idparc VARCHAR)
-    RETURNS TABLE(nom VARCHAR,
-                  ini_instru VARCHAR)
-AS $$
+CREATE OR REPLACE FUNCTION urba_foncier.rm_intersect_ediparc_adssecteurinstruction2(IN idparc character varying)
+  RETURNS TABLE(nom character varying, ini_instru character varying) AS
+$BODY$
+	 
 BEGIN
     RETURN QUERY SELECT b.nom, b.ini_instru
-                 FROM  cadastre.edi_parc  AS a , urba_foncier.ads_secteur_instruction AS b
-                 WHERE a.id_parc = idParc AND st_intersects(a.shape, b.shape);
+                FROM cadastre_qgis.geo_parcelle AS a , urba_foncier.ads_secteur_instruction AS b
+                WHERE a.geo_parcelle = idParc AND st_intersects(a.geom, b.shape);
 END;
-$$
-LANGUAGE 'plpgsql';
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
 
 --function intersect cadastre.edi_parc &&  urba_foncier.v_ads_autorisation
-CREATE OR REPLACE FUNCTION urba_foncier.intersect_ediparc_vadsautorisation(idparc VARCHAR)
-    RETURNS TABLE(numdossier VARCHAR)
-AS $$
+CREATE OR REPLACE FUNCTION urba_foncier.rm_intersect_ediparc_vadsautorisation2(IN idparc character varying)
+  RETURNS TABLE(numdossier character varying) AS
+$BODY$
 BEGIN
     RETURN QUERY SELECT b.numdossier
-                 FROM cadastre.edi_parc AS a , urba_foncier.v_ads_autorisation AS b
-                 WHERE a.id_parc = idParc AND st_intersects(a.shape, b.shape);
+FROM cadastre_qgis.geo_parcelle AS a , urba_foncier.v_ads_autorisation AS b
+WHERE a.geo_parcelle = idParc AND st_intersects(a.geom, b.shape);
 END;
-$$
-LANGUAGE 'plpgsql';
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
